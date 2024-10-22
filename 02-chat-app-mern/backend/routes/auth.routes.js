8df750fa5a2a4890182b33657b7bcb13de728a0e
shivsundar3009 <shivsundar3009@gmail.com>
2024-10-22 13:11:32 +0530

    changes in ideas chat app deletion

--- 02-chat-app-mern/backend/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-
-import express from 'express'
-import { registerUser } from '../controllers/user.controllers.js'
-
-const router = express.Router()
-
-router.post("/signup", registerUser)
-
-export default router
